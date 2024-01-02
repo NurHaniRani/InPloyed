0d135afce1db1c0921b977e17151fcb3ea76292f
NurHaniRani <134916819+NurHaniRani@users.noreply.github.com>
2024-01-02 23:12:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="icon" href="src/inployed.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> InPloyed | Home</title> 
+    <link rel="stylesheet" href="css/home2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+   </head>
+<body>
+  <div class="img">
+  <div class="left">
+    <img src="src/logo.png">
+    <div class="title">InPloyed</div>
+    <div class="sub_title">Admin Managing Site</div>
+    <div class="btns">
+    <a href="admin/login.php">
+      <button>Login</button> </a>
+    </div>
+  </div>
+  <div class="right">
+        <img src="src/Image 54.png">
+  </div>
+</div>
+</body>
+</html>
