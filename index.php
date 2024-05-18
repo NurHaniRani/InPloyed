095eed2abe5eadd32374044212c20095469c4122
NurHaniRani <134916819+NurHaniRani@users.noreply.github.com>
2024-05-19 00:28:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <!DOCTYPE html>
-
 <html lang="en" dir="ltr">
-  <head>
-    <link rel="icon" href="src/inployed.png">
+<head>
+    <link rel="icon" href="src/logoSnipped.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> InPloyed | Home</title> 
-    <link rel="stylesheet" href="css/home2.css">
+    <title>CareerConnect</title> 
+    <link rel="stylesheet" href="css/home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-   </head>
+</head>
 <body>
-  <div class="img">
-  <div class="left">
-    <img src="src/logo.png">
-    <div class="title">InPloyed</div>
-    <div class="sub_title">Admin Managing Site</div>
-    <div class="btns">
-    <a href="admin/login.php">
-      <button>Login</button> </a>
+<div class="img">
+    <div class="centered-content">
+        <div class="logo">
+            <img src="src/logoSnipped.png" alt="Logo">
+        </div>
+        <div class="left">
+            <div class="sub_title">Managing Site</div>
+            <div class="btns">
+                <a href="admin/login.php"><button>Login</button></a>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="right">
-        <img src="src/Image 54.png">
-  </div>
 </div>
 </body>
 </html>
